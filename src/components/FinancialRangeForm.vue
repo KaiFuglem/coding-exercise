eb75c13a17fd021a5075abcebaf0fa7c319f1450
Kai Fuglem <kaifuglemphotos@gmail.com>
2019-03-20 00:08:15 -0700

    Committing my changes for the coding exercise

---
+++ src/components/FinancialRangeForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="range-input">
+    <label for="">
+      <input />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RangeInput',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>

9a0018b600bc95f55655f5a471def1f75daf3551
Kai Fuglem <kaifuglemphotos@gmail.com>
2019-03-20 08:30:43 -0700

    Added more tests

--- src/components/__tests__/FinancialRangeForm.test.js
+++ src/components/__tests__/FinancialRangeForm.test.js
@@ -28,4 +28,92 @@ describe('FinancialRangeForm component', () => {
     expect(expandedValue).toBe('120000000');
   });
 
+  test('computedMin returns NaN for strings', () => {
+    // Arrange + Act
+    const wrapper = mount(FinancialRangeForm, {
+      data() {
+        return {
+          minValue: 'test',
+        };
+      },
+    });
+    const computedMin = wrapper.vm.computedMin;
+    // Assert
+    expect(Number.isNaN(computedMin)).toBe(true);
+  });
+
+  test('computedMin returns Number for valid numbers', () => {
+    // Arrange + Act
+    const wrapper = mount(FinancialRangeForm, {
+      data() {
+        return {
+          minValue: '123.8',
+        };
+      },
+    });
+    const computedMin = wrapper.vm.computedMin;
+    // Assert
+    expect(Number.isNaN(computedMin)).toBe(false);
+    expect(computedMin).toBe(123.8);
+  });
+
+  test('computedMin returns correct Number for shorthand numbers', () => {
+    // Arrange + Act
+    const wrapper = mount(FinancialRangeForm, {
+      data() {
+        return {
+          minValue: '123.8k',
+        };
+      },
+    });
+    const computedMin = wrapper.vm.computedMin;
+    // Assert
+    expect(Number.isNaN(computedMin)).toBe(false);
+    expect(computedMin).toBe(123800);
+  });
+
+  test('computedMax returns NaN for strings', () => {
+    // Arrange + Act
+    const wrapper = mount(FinancialRangeForm, {
+      data() {
+        return {
+          maxValue: '12s',
+        };
+      },
+    });
+    const computedMax = wrapper.vm.computedMax;
+    // Assert
+    expect(Number.isNaN(computedMax)).toBe(true);
+  });
+
+  test('computedMax returns Number for valid numbers', () => {
+    // Arrange + Act
+    const wrapper = mount(FinancialRangeForm, {
+      data() {
+        return {
+          maxValue: '123.8999',
+        };
+      },
+    });
+    const computedMax = wrapper.vm.computedMax;
+    // Assert
+    expect(Number.isNaN(computedMax)).toBe(false);
+    expect(computedMax).toBe(123.8999);
+  });
+
+  test('computedMax returns correct Number for shorthand numbers', () => {
+    // Arrange + Act
+    const wrapper = mount(FinancialRangeForm, {
+      data() {
+        return {
+          maxValue: '50000m',
+        };
+      },
+    });
+    const computedMax = wrapper.vm.computedMax;
+    // Assert
+    expect(Number.isNaN(computedMax)).toBe(false);
+    expect(computedMax).toBe(50000000000);
+  });
+
 });
